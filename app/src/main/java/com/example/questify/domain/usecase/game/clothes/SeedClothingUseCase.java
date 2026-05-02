2abc6f227c1fbbf40089e709e3f90ebd7c8a2291
ulyanakozlova <ulianka.kozlova@gmail.com>
2026-05-02 22:52:55 +0300

    Change color theme + add clothes

--- app/src/main/java/com/example/questify/domain/usecase/game/clothes/SeedClothingUseCase.java
+++ app/src/main/java/com/example/questify/domain/usecase/game/clothes/SeedClothingUseCase.java
@@ -22,11 +22,23 @@ public class SeedClothingUseCase {
         if (existingCount >= CLOTHING_COUNT) {
             return;
         }
-
+// todo
         Object[][] items = {
-                {"Шляпа", 50, R.drawable.pet_hat},
-                {"Шарф", 30, R.drawable.pet_scarf},
-                {"Очки", 40, R.drawable.pet_glasses}
+                {"black dress", 120, R.drawable.cat_in_black_dress},
+                {"green suit", 30, R.drawable.cat_in_green_suit},
+                {"sport suit", 40, R.drawable.cat_in_sport_suit},
+                {"brown coat", 60, R.drawable.cat_with_brown_coat},
+                {"corset_dress", 50, R.drawable.cat_with_corset_dress},
+                {"denim overalls", 30, R.drawable.cat_with_denim_overalls},
+                {"evening dress", 70, R.drawable.cat_with_evening_dress},
+                {"jeans and t-shirt", 25, R.drawable.cat_with_jeans_and_tshirt},
+                {"leather pants and blue sweater", 30, R.drawable.cat_with_leather_pants_blue_sweater},
+                {"pants and shirt", 35, R.drawable.cat_with_pants_and_shirt},
+                {"pink dress", 90, R.drawable.cat_with_pink_dress},
+                {"purple dress", 80, R.drawable.cat_with_purple_dress},
+                {"red skirt and black top", 30, R.drawable.cat_with_red_skirt_black_top},
+                {"shorts and tanktop", 45, R.drawable.cat_with_shorts_and_tanktop},
+                {"white t-shirt and jeans", 40, R.drawable.cat_with_white_t_shirt_and_jeans}
         };
 
         for (Object[] item : items) {
